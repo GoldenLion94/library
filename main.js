05e8f0fed237d1c668a39cb9c7b831eb994c2a44
Milton Sanchez <miltonsanchez5081@gmail.com>
2023-05-29 09:41:07 -0400

    Add JS Logic & CSS Styling

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ newBookBtn.addEventListener("click", function () {
 });
 
 // FORM INPUT PULL (Linked to HTML DOM & JS function's [myLibrary] [render])
+
 function addBookToLibrary() {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
@@ -28,7 +29,7 @@ function addBookToLibrary() {
   render();
 }
 
-// NEW BOOK SUBMIT-BUTTON (Linked to JS function [addBookToLibrary])
+// NEW BOOK SUBMIT-BUTTON (Linked to JS function [addBookToLibrary] & HTML Form via DOM) - Event Listener Added to form.
 document
   .querySelector("#new-book-form")
   .addEventListener("submit", function (event) {
@@ -40,12 +41,46 @@ document
 
 function render() {
   let libraryElement = document.querySelector("#library");
+
   libraryElement.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
     let bookElement = document.createElement("div");
-    bookElement.innerHTML = `<p>${book.title}  ${book.author} ${book.pages}
-   <p>`;
+    // CSS
+    bookElement.setAttribute("class", "book-card");
+    bookElement.innerHTML = `
+    <div class="card-header">
+  <h3 class="tittle">${book.title}</h3>
+  <h5 class="author">by ${book.author}</h5>
+</div>
+
+<div class="card-body">
+  <p>${book.pages} pages</p>
+  <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+  <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+</div>
+<button class="toggle-read-btn"  onclick="toggleRead(${i})">Toggle Read</button>
+</div>
+    `;
     libraryElement.appendChild(bookElement);
   }
+}
+
+// REMOVE BUTTON (To remove Books)
+
+function removeBook(i) {
+  myLibrary.splice(i, 1);
+  render();
+}
+// Create Prototype Toggle function
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+// Invoking constructor "Book" prototype method "toggleRead"
+
+function toggleRead(i) {
+  myLibrary[i].toggleRead();
+  render();
 }
